925527dde6992716d72b37bd3e0e2029511a3a88
Martin Carolan <mail@mcarolan.net>
2014-05-11 23:09:26 +0100

    Implement tests

--- src/main/scala/net/mcarolan/Palindromes.scala
+++ src/main/scala/net/mcarolan/Palindromes.scala
@@ -7,19 +7,28 @@ object Palindromes extends App {
 
   private[this] def isPalindrome(s: String) = s == s.reverse
 
-  def getLongestSubPalindromes(s: String, n: Int): Seq[(String, PalindromeLength, PalindromeOffset)] = {
+  case class SubPalindromeResult(palindrome: String, index: Int, length: Int) {
+    override def toString = s"Text: $palindrome, Index: $index, Length: $length"
+  }
+
+  def getLongestSubPalindromes(s: String, n: Int): Seq[SubPalindromeResult] = {
     def palindromesFromPosition(startAt: Int) = {
       val subStringsFromPosition = (startAt + 2 to s.length).map(s.substring(startAt, _))
 
       for {
         p <- subStringsFromPosition if isPalindrome(p)
       }
-        yield (p, p.length, startAt)
+        yield SubPalindromeResult(p, startAt, p.length)
     }
 
-    val allPalindromes = (0 until s.length - 1).flatMap(palindromesFromPosition)
-    allPalindromes.sortBy(- _._2) take n
+    val allPalindromes = (0 until s.length).flatMap(palindromesFromPosition)
+    allPalindromes sortBy(- _.length) take n
   }
 
-  println("Hello, palindromes")
+  val argsMap = args.zipWithIndex.map(_.swap).toMap
+
+  val input = argsMap.getOrElse(0, "sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop")
+  val numberToReturn = argsMap.getOrElse(1, "3").toInt
+
+  println(getLongestSubPalindromes(input, numberToReturn).mkString("\n"))
 }
--- src/test/scala/net/mcarolan/PalindromesSpec.scala
+++ src/test/scala/net/mcarolan/PalindromesSpec.scala
@@ -6,14 +6,18 @@ import org.scalacheck.Prop._
 case class PalindromesSpec() extends Specification with ScalaCheck {
 
   import Generators._
+  import Palindromes.SubPalindromeResult
 
   def is = s2"""Palindromes
     Longest palindrome of a palindrome should be itself        $longestPalindromeOfPalindrome
-    Example 1                                                  $example1
+    Example 1  - sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop                                                $example1
              """
 
   def longestPalindromeOfPalindrome = forAllNoShrink(palindromeGen) { palindrome =>
-    (palindrome, palindrome.length, 0) must be_==(Palindromes.getLongestSubPalindromes(palindrome, 1).head)
+    SubPalindromeResult(palindrome, 0, palindrome.length) must be_==(Palindromes.getLongestSubPalindromes(palindrome, 1).head)
   }
+
+  def example1 = 
+    Vector(SubPalindromeResult("hijkllkjih", 23, 10), SubPalindromeResult("defggfed", 13, 8), SubPalindromeResult("ijkllkji", 24, 8)) must be_==(Palindromes.getLongestSubPalindromes("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop", 3))
 
 }
